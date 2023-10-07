eddecb21d49cc79cbfdb861e2c44ff8c6054debf
Suriya Kenma <bekzerker@gmail.com>
2023-10-07 20:19:49 +0700

    learn using tween to tweak some value create transition or some basic animation without animation player it really useful

--- scrpits/houses/house_orange.gd
+++ scrpits/houses/house_orange.gd
@@ -1,5 +1,10 @@
 extends StaticBody2D
 
+signal player_entered
+signal player_exited
 
-func _on_area_2d_body_entered(body):
-	print(body)
+func _on_area_2d_body_entered(_body):
+	player_entered.emit()
+	
+func _on_area_2d_body_exited(_body):
+	player_exited.emit()
--- scrpits/levels/level.gd
+++ scrpits/levels/level.gd
@@ -3,9 +3,13 @@ extends Node2D
 @onready var player: CharacterBody2D = $player
 var bullet_scenes: PackedScene = preload("res://scenes/projectiles/bullet.tscn")
 var grenade_scenes: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+var camera_scenes: PackedScene = preload("res://scenes/players/camera.tscn")
+
+var player_camera: Camera2D
 
 func _ready():
-	pass # Replace with function body.
+	player_camera = camera_scenes.instantiate() as Camera2D
+	player.add_child(player_camera)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,3 +33,15 @@ func _on_player_grenade(marker_position: Vector2, direction: Vector2):
 	grenade.linear_velocity = direction * 1000
 	$projectiles.add_child(grenade)
 	pass
+
+
+func _on_house_orange_player_entered():
+	var tween: Tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(player,'speed',0,0.1)
+	tween.tween_property(player_camera, 'zoom', Vector2(1,1), 1)
+
+
+func _on_house_orange_player_exited():
+	var tween: Tween = create_tween()
+	tween.tween_property(player_camera, 'zoom', Vector2(0.8,0.8), 1)
